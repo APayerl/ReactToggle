2b3a13871d8b5a617ae68b62a2703595856169bf
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2020-04-23 08:48:47 +0200

    master - first push?

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
+import { Component } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Toggle extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			toggledOn: false,
+			base: {
+				height: -1,
+				width: -1,
+				borderRadius: -1
+			},
+			slider: {
+				height: -1,
+				width: -1,
+				borderRadius: -1
+			}
+		};
+	}
+
+	componentDidMount() {
+		const bHeight = this.baseElement.clientHeight;
+		const bWidth = this.baseElement.clientHeight;
+		const bBorderRadius = this.baseElement.style.borderRadius;
+		const sHeight = this.sliderElement.clientHeight;
+		const sWidth = this.sliderElement.clientHeight;
+		const sBorderRadius = this.sliderElement.style.borderRadius;
+
+		console.log(bHeight, bWidth, bBorderRadius);
+		console.log(sHeight, sWidth, sBorderRadius);
+		this.setState({ base: { height: bHeight, bWidth: bWidth, borderRadius: bBorderRadius }, slider: { height: sHeight, width: sWidth, borderRadius: sBorderRadius }});
+	}
+
+	calculatePadding() {
+		const height = this.state.base.height;
+		let value = 5;
+		if(height < 50) {
+			value = height <= 20 ? 2 : Math.trunc(height / 10);
+		}
+		return value + '%';
+	}
+
+	handleClick(event) {
+		let toggled = this.state.toggledOn;
+		if(event.target.classList.contains('enabled')) {
+			event.target.classList.replace('enabled', 'disabled');
+			this.setState({ toggledOn: false });
+			toggled = !toggled;
+		} else {
+			event.target.classList.replace('disabled', 'enabled');
+			this.setState({ toggledOn: true });
+			toggled = !toggled;
+		}
+		console.log(toggled);
+	}
+
+	render() {
+		return (
+			<div 
+				id="base" 
+				className="switch" 
+				style={{
+					backgroundColor: this.state.toggledOn ? 'green' : 'gray',
+					padding: this.calculatePadding() }}
+				ref={ (divElement) => { this.baseElement = divElement } }>
+				<button 
+					className="slider disabled" 
+					onClick={this.handleClick.bind(this)}
+					ref={ (divElement) => { this.sliderElement = divElement } }></button>
+			</div>
+		);
+	}
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Toggle />,
+	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
